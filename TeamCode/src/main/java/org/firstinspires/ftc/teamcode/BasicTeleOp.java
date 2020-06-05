76d2e90d23f3e1909f17c0261c7ebff076cdc6d4
NithikY <nithikreddy@gmail.com>
2020-06-04 19:02:30 -0500

    Update BasicTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -40,7 +40,7 @@ public class BasicTeleOp extends LinearOpMode {
 
         r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);//r is the
         double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-        rightX = gamepad1.right_stick_x * -1;
+        rightX = gamepad1.right_stick_x * -0.5;
         velocityConst = 1.414213565;
         vFL = (r * Math.sin(robotAngle) + rightX)* velocityConst;
         vFR = (r * Math.cos(robotAngle) - rightX)* velocityConst;
