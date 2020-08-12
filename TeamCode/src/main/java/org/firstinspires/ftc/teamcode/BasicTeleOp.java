22dc9f96baf701d7a5a411fe5aceb3dfcc98380a
AbhiramPendela <66391169+AbhiramPendela@users.noreply.github.com>
2020-08-12 15:28:30 -0500

    some syntax changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -12,7 +12,6 @@ import java.lang.*;
 @TeleOp (name = "TeleOp Mec Wheels", group = "Basic")
 public class BasicTeleOp extends LinearOpMode {
     //I declare all the motors over here.
-    private ElapsedTime runtime = new ElapsedTime();
 
     private DcMotor motorFL;
     private DcMotor motorFR;
@@ -40,31 +39,27 @@ public class BasicTeleOp extends LinearOpMode {
 
     //uses if statements to make sure that the motors don't burn out and sets it equal to 1 if it is over 1 or equal to -1 if it is below -1
     public void restrictPower(double powerNum) {
-        if(vBR > powerNum) {
+        if (vBR > powerNum) {
             vBR = powerNum;
-        }
-        else if(vBR < -powerNum) {
+        } else if (vBR < -powerNum) {
             vBR = -powerNum;
         }
 
-        if(vBL > powerNum) {
+        if (vBL > powerNum) {
             vBL = powerNum;
-        }
-        else if(vBL < -powerNum) {
+        } else if (vBL < -powerNum) {
             vBL = -powerNum;
         }
 
-        if(vFR > powerNum) {
+        if (vFR > powerNum) {
             vFR = powerNum;
-        }
-        else if(vFR < -powerNum) {
+        } else if (vFR < -powerNum) {
             vFR = -powerNum;
         }
 
-        if(vFL > powerNum) {
+        if (vFL > powerNum) {
             vFL = powerNum;
-        }
-        else if(vFL < -powerNum) {
+        } else if (vFL < -powerNum) {
             vFL = -powerNum;
         }
 
@@ -81,26 +76,25 @@ public class BasicTeleOp extends LinearOpMode {
         double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
 
         //test code
-        if(gamepad1.left_stick_y > 0.8) {
+        if (gamepad1.left_stick_y > 0.8) {
             rightX = gamepad1.right_stick_x * -0.5;
         } else {
             rightX = gamepad1.right_stick_x * -0.25;
         }
 
         //used to counteract the rotation from strafing
-        if(Math.abs(gamepad1.left_stick_y) < 0.1 && gamepad1.left_stick_x > 0){
+        if (Math.abs(gamepad1.left_stick_y) < 0.1 && gamepad1.left_stick_x > 0) {
             rightX -= strafeCounter * Math.abs(gamepad1.left_stick_x);
-        }
-        else if(Math.abs(gamepad1.left_stick_y) < 0.1 && gamepad1.left_stick_x < 0){
+        } else if (Math.abs(gamepad1.left_stick_y) < 0.1 && gamepad1.left_stick_x < 0) {
             rightX += strafeCounter * Math.abs(gamepad1.left_stick_x);
         }
 
         //sets the power for each of the motor variables
         velocityConst = 1.414213565;
-        vFL = (r * Math.sin(robotAngle) + rightX)* velocityConst;
-        vFR = (r * Math.cos(robotAngle) - rightX)* velocityConst;
-        vBL = (r * Math.cos(robotAngle) + rightX)* velocityConst;
-        vBR = (r * Math.sin(robotAngle) - rightX)* velocityConst;
+        vFL = (r * Math.sin(robotAngle) + rightX) * velocityConst;
+        vFR = (r * Math.cos(robotAngle) - rightX) * velocityConst;
+        vBL = (r * Math.cos(robotAngle) + rightX) * velocityConst;
+        vBR = (r * Math.sin(robotAngle) - rightX) * velocityConst;
 
         restrictPower(1);
 
@@ -132,49 +126,49 @@ public class BasicTeleOp extends LinearOpMode {
         //left_stick_x shows the movement across the horizontal axis, left or right
 
         //On game controller the joystick all the way up is -1.0 and all the way down is 1.0
-        if(Math.abs(gamepad1.left_stick_x) < 0.10) { //this if statement is saying "if the stick_x is closer to zero(middle) then use the left stick y value to go forward"
+        if (Math.abs(gamepad1.left_stick_x) < 0.10) { //this if statement is saying "if the stick_x is closer to zero(middle) then use the left stick y value to go forward"
             //purpose of the 0.10 is to make sure for the slightest touch is accommodated in order for the robot to go forward and not left and right
             vFL = gamepad1.left_stick_y; //will go forward/backwards
             vFR = gamepad1.left_stick_y; //will go forward/backwards
             vBL = gamepad1.left_stick_y; //will go forward/backwards
             vBR = gamepad1.left_stick_y; //will go forward/backwards
-        } else if(Math.abs(gamepad1.left_stick_y) < 0.10) { //This else if statement is used in order to go sideways. The front right and the backLeft(opposite of each other) must go in different directions(move away) in order to go sideways
+        } else if (Math.abs(gamepad1.left_stick_y) < 0.10) { //This else if statement is used in order to go sideways. The front right and the backLeft(opposite of each other) must go in different directions(move away) in order to go sideways
             //We used the left_stick_x in the game-pad in order for the robot to go in the left or right movement, through the horizontal axis
             //if hte joystick is going to the right or the left it will set the motors to that value
             vFL = gamepad1.left_stick_x;
             vFR = gamepad1.left_stick_x * -1;
             vBL = gamepad1.left_stick_x * -1;
             vBR = gamepad1.left_stick_x;
-        } else if(gamepad1.left_stick_x > 0 && gamepad1.left_stick_y > 0){ //The diagonal movement can be shown here with two wheels moving. The front left and the back right.
+        } else if (gamepad1.left_stick_x > 0 && gamepad1.left_stick_y > 0) { //The diagonal movement can be shown here with two wheels moving. The front left and the back right.
             //We use the sum of the left stick x and y and get the average to find the speed diagonally
             //finding the average determines speed diagonally
             //EX: the direction of joystick is facing northeast
             vFL = Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
             vBR = Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
-        } else if(gamepad1.left_stick_x < 0 && gamepad1.left_stick_y > 0){
+        } else if (gamepad1.left_stick_x < 0 && gamepad1.left_stick_y > 0) {
             //the direction of the joystick is facing northwest
             //will go diagonal in the forward left direction
             vFR = Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
             vBL = Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
-        } else if(gamepad1.left_stick_x > 0 && gamepad1.left_stick_y < 0){
+        } else if (gamepad1.left_stick_x > 0 && gamepad1.left_stick_y < 0) {
             //the position of the joystick is southeast
             //the robot will go bottom right diagonal
             vFL = -Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
             vBR = -Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
-        } else if(gamepad1.left_stick_x < 0 && gamepad1.left_stick_y < 0){
+        } else if (gamepad1.left_stick_x < 0 && gamepad1.left_stick_y < 0) {
             //the position of the joystick is southwest
             //the robot will go bottom left diagonal
             vFR = -Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
             vBL = -Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
         }
         //perfect strafe
-        if(gamepad1.right_bumper){
+        if (gamepad1.right_bumper) {
             vFL = 0.5;
             vFR = -0.5;
             vBL = -0.5;
             vBR = 0.5;
         }
-        if(gamepad1.left_bumper){
+        if (gamepad1.left_bumper) {
             vFL = -0.5;
             vFR = 0.5;
             vBL = 0.5;
@@ -229,17 +223,14 @@ public class BasicTeleOp extends LinearOpMode {
 //        }
 
 
-
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        central  = hardwareMap.get(DcMotor.class, "central");
-        servo1 = hardwareMap.get(Servo.class,"servo1" );
-
-
+        central = hardwareMap.get(DcMotor.class, "central");
+        servo1 = hardwareMap.get(Servo.class, "servo1");
 
 
         // Wait for the game to start (driver presses PLAY)
@@ -253,10 +244,10 @@ public class BasicTeleOp extends LinearOpMode {
 
             double motorPower = -gamepad1.left_stick_y;
 
-            if(gamepad1.dpad_up){
+            if (gamepad1.dpad_up) {
                 servoPosition = 0.5;
             }
-            if(gamepad2.dpad_down){
+            if (gamepad2.dpad_down) {
                 servoPosition = -0.5;
             }
 
@@ -270,6 +261,7 @@ public class BasicTeleOp extends LinearOpMode {
             telemetry.addData("Motors", "Motor Power:", motorPower);
             telemetry.addData("Servo", "Servo Position:", servoPosition);
             telemetry.update();
-    }
+        }
 
+    }
 }
